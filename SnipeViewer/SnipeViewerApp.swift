4b1a356a48d81d2cb79d11bb027d7fa1d8fb7c45
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-11-17 18:02:26 -0600

    Initial Commit

---
+++ SnipeViewer/SnipeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipeViewerApp.swift
+//  SnipeViewer
+//
+//  Created by Emilio Marin on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SnipeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
